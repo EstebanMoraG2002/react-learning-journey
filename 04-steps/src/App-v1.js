018d9ef8c3925340f98751f09d07d076a277a801
Esteban Mora <estebanjmora16@gmail.com>
2025-09-13 18:20:22 -0600

    refactor(steps): create reusable Button component with customizable styles and icons

--- 04-steps/src/App-v1.js
+++ 04-steps/src/App-v1.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <div>
       <Steps />
-      {/*<Steps />*/}
+      <Steps />
     </div>
   );
 }
@@ -45,25 +45,19 @@ function Steps() {
           </p>
 
           <div className="buttons">
-            <Button bgColor="#7950f2" onClick={handlePrevious} textColor="#fff">
-              <spam>👈</spam> Previous
-            </Button>
-            <Button bgColor="#7950f2" onClick={handleNext} textColor="#fff">
-              Next <spam>👉</spam>
-            </Button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}>
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}>
+              Next
+            </button>
           </div>
         </div>
       )}
     </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}>
-      {children}
-    </button>
   );
 }
